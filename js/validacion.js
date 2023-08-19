8a887ba8b259de2f720421be9955bb637aa474b8
jhasid <jhasid214@gmail.com>
2023-08-19 11:44:10 -0500

    En desarrollo

---
+++ js/validacion.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', validateForm);
+
+function validateForm(event) {
+  event.preventDefault();
+  const email = document.querySelector('#correo').value;
+  const password = document.querySelector('#contrasena').value;
+  
+  // Validar el correo electrónico
+  if (!email) {
+    alert('Por favor ingresa tu correo electrónico');
+    return false;
+  }
+
+  if (email.length == "") {
+    alert('ingrese un correo electronico');
+    return false;
+  }
+  
+  // Validar la contraseña
+  if (!password) {
+    alert('Por favor ingresa tu contraseña');
+    return false;
+  }
+  if (password.length < 8) {
+    alert('La contraseña debe tener al menos 8 caracteres');
+    return false;
+  }
+  
+  // Si todo está bien, redirigir al usuario a la página de inicio
+  window.location.href = 'dashboard.html';
+}
+
+
