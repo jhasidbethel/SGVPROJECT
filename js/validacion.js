204b2fadc373f11cf5fa72d6b585e9af0f01916b
jhasid <jhasid@debian.debian12>
2023-10-15 23:22:46 -0500

    se añadieron funcionalidades

---
+++ js/validacion.js
@@ -0,0 +1,164 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', validateForm);
+
+function validateForm(event) {
+  event.preventDefault();
+  const email = document.querySelector('#correo').value;
+  const password = document.querySelector('#contrasena').value;
+  
+  // Validar el correo electrónico
+  if (!email) {
+    alert('Por favor ingresa tu correo electrónico');
+    return false;
+  }
+
+  if (email.length == "") {
+    alert('ingrese un correo electronico');
+    return false;
+  }
+  
+  // Validar la contraseña
+  if (!password) {
+    alert('Por favor ingresa tu contraseña');
+    return false;
+  }
+  if (password.length < 8) {
+    alert('La contraseña debe tener al menos 8 caracteres');
+    return false;
+  }
+  
+  
+  window.location.href = 'dashboard.html';
+}
+
+// dashboard 
+
+function showOptionTab() {
+  let optionTab = document.getElementById("option-tab");
+  if (optionTab.style.display === "block") {
+     optionTab.style.display = "none";
+  } else {
+     optionTab.style.display = "block";
+  }
+}
+
+function goToFile(file) {
+ window.location.href = file;
+}
+
+
+// funcionalidad a la tabla de producto //
+
+let rIndex,
+
+table = document.getElementById("table");
+
+// check the empty input
+
+function checkEmptyInput() {
+  let vacio = false,
+  Nombre = document.getElementById("Nombre").value,
+  Codigo = document.getElementById("Codigo").value,
+  Descripcion = document.getElementById("Descripcion").value;
+  Categoria = document.getElementById("Categoria").value;
+  Almacen = document.getElementById("Almacen").value;
+  
+  if(Nombre === ""){
+    alert("Por favor llene el campo de nombre");
+    vacio = true;
+  }
+  else if(Codigo === ""){
+    alert("Por favor llene el campo de codigo");
+    vacio = true;
+  }
+  else if(Descripcion === ""){
+    alert("Porfavor llene el campo de descripcion");
+    vacio = true;
+  }
+  else if(Categoria === ""){
+    alert("Por favor llene el campo de categoria");
+    vacio = true;
+  }
+  else if(Almacen === ""){
+    alert("Por favor llene el campo de almacen");
+    vacio = true;
+  }
+  return vacio;
+}
+
+// add Row
+
+function addHtmlTableRow() {
+  
+  // get the table by id
+  // create a new row and cells
+  // get value from input text
+  // set the values into row cell's
+  if(!checkEmptyInput()){
+    let newRow = table.insertRow(table.length),
+    cell1 = newRow.insertCell(0),
+    cell2 = newRow.insertCell(1),
+    cell3 = newRow.insertCell(2),
+    cell4 = newRow.insertCell(3),
+    cell5 = newRow.insertCell(4),
+    
+    Nombre = document.getElementById("Nombre").value,
+    Codigo = document.getElementById("Codigo").value,
+    Descripcion = document.getElementById("Descripcion").value,
+    Categoria = document.getElementById("Categoria").value, 
+    Almacen = document.getElementById("Almacen").value;
+    
+    cell1.innerHTML = Nombre;
+    cell2.innerHTML = Codigo;
+    cell3.innerHTML = Descripcion;
+    cell4.innerHTML = Categoria;
+    cell5.innerHTML = Almacen;
+    // call the function to set the event to the new row
+    
+    selectedRowToInput();}
+  
+  }
+  
+  function selectedRowToInput() {
+    
+    for(let i = 1; i < table.rows.length; i++)
+    {
+      table.rows[i].onclick = function() {
+        rIndex = this.rowIndex;
+        document.getElementById("Nombre").value = this.cells[0].innerHTML;
+        document.getElementById("Codigo").value = this.cells[1].innerHTML;
+        document.getElementById("Descripcion").value = this.cells[2].innerHTML;
+        document.getElementById("Categoria").value = this.cells[2].innerHTML;
+        document.getElementById("Almacen").value = this.cells[2].innerHTML;
+      
+      };
+    }
+  }
+  
+  selectedRowToInput();
+  
+  function editHtmlTbleSelectedRow() {
+    let Nombre = document.getElementById("Nombre").value,
+    Codigo = document.getElementById("Codigo").value,
+    Descripcion = document.getElementById("Descripcion").value,
+    Categoria = document.getElementById("Categoria").value,
+    Almacen = document.getElementById("Almacen").value;
+    if(!checkEmptyInput()){
+      table.rows[rIndex].cells[0].innerHTML = Nombre;
+      table.rows[rIndex].cells[1].innerHTML = Codigo;
+      table.rows[rIndex].cells[2].innerHTML = Descripcion;
+      table.rows[rIndex].cells[3].innerHTML = Categoria;
+      table.rows[rIndex].cells[4].innerHTML = Almacen;
+    }
+  }
+  
+  function removeSelectedRow() {
+    table.deleteRow(rIndex);
+    
+    document.getElementById("Nombre").value = "";
+    document.getElementById("Codigo").value = "";
+    document.getElementById("Descripcion").value = "";
+    document.getElementById("Categoria").value = "";
+    document.getElementById("Almacen").value = "";
+  }
+
